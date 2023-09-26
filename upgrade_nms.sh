0431812c416df60b1baa81865e2b22ae2609aaca
seshAlkimi <125999511+seshAlkimi@users.noreply.github.com>
2023-09-26 14:21:21 +0100

    Update upgrade_nms.sh

--- upgrade_nms.sh
+++ upgrade_nms.sh
@@ -3,7 +3,7 @@ shopt -s extglob  ## this is to add exlude a folder in a cp command
 cd /home/ubuntu/node_setup
 docker-compose down
 server_pid="$(<webserver_pid.txt)"
-echo "SERVICER PID IS $server_pid"
+kill -9 $server_pid
 folder_name="nms"$(date +%Y%m%d%H%M%S)
 mkdir -p backup/"$folder_name"
 cp -rp /home/ubuntu/node_setup/!(backup) backup/"$folder_name"
