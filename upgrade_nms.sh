35efcbebab3d9fc4b62c0aa02ce25a0c34657419
seshAlkimi <125999511+seshAlkimi@users.noreply.github.com>
2023-09-27 12:44:28 +0100

    Update upgrade_nms.sh

--- upgrade_nms.sh
+++ upgrade_nms.sh
@@ -1,17 +1,16 @@
-#! /bin/bash
+#! /bin/bash -xv
 shopt -s extglob  ## this is to add exlude a folder in a cp command
 cd /home/ubuntu/node_setup
 docker-compose down
 pkill -f "nms_web_server" 
 folder_name="nms"$(date +%Y%m%d%H%M%S)
-mkdir -p backup/"$folder_name"
-cp -rp /home/ubuntu/node_setup/!(backup) backup/"$folder_name"
-git clean  -d  -f .
+mkdir -p /home/ubuntu/node_backup/"$folder_name"
+cp -rp /home/ubuntu/node_setup/ /home/ubuntu/node_backup/"$folder_name"
+git checkout .
 git pull
-wget --backups=1 https://d1xjh92lb8fey3.cloudfront.net/NMS-Update/dev/nms_web_server
+wget  https://d1xjh92lb8fey3.cloudfront.net/NMS-Update/dev/nms_web_server
 chmod 755 nms_web_server
 nohup ./nms_web_server &
-echo $! > webserver_pid.txt
 docker-compose up -d
-echo "NMS Upgrade completed Successfully" > backup/nms_upgrade.log
+echo "NMS Upgrade completed Successfully" > /home/ubuntu/node_backup/nms_upgrade.log
 chmod 755 upgrade_nms.sh
