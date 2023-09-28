1ab4b4800cdb2f7be19594c0a12c703076ce8c1f
PremUpadhyay55 <prem.upadhyay@kensaltensi.org>
2023-09-28 20:21:24 +0530

    remove sleep

--- upgrade_nms.sh
+++ upgrade_nms.sh
@@ -11,7 +11,6 @@ git pull
 wget  https://d1xjh92lb8fey3.cloudfront.net/NMS-Update/dev/nms_web_server
 chmod 755 nms_web_server
 docker-compose up -d
-sleep 10
 chmod 755 upgrade_nms.sh
 nohup ./nms_web_server > nms_web_server.log 2>&1 &
 echo "NMS Upgrade completed Successfully" > /home/ubuntu/node_backup/nms_upgrade.log
