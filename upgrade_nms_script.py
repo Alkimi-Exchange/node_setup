cf49ff2d54dd93caf74addc2555f904764d093f4
PremUpadhyay55 <prem.upadhyay@kensaltensi.org>
2023-10-04 12:42:08 +0530

    upgrade nms script added

---
+++ upgrade_nms_script.py
@@ -0,0 +1,115 @@
+import http.server
+import platform
+import shutil
+import socketserver
+import os
+import subprocess
+import psutil
+import json
+import time 
+import threading
+import multiprocessing
+from http.server import BaseHTTPRequestHandler
+
+
+def send_response(handler, status_code, message):
+    # Send the HTTP response status code
+    handler.send_response(status_code)
+
+    # End the HTTP headers
+    handler.end_headers()
+
+    # Create a response body as a dictionary
+    response_body = {
+        'success': True if status_code == 201 else False,
+        'message': message
+    }
+
+    # Convert the response body to a JSON string
+    response_json = json.dumps(response_body)
+
+    # Write the JSON response to the client
+    handler.wfile.write(bytes(response_json, 'utf-8'))
+
+
+def command_run(command):
+    try:
+        # Execute the download command using subprocess
+        result = subprocess.run(command, shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        # Check the return code to see if the command was successful
+        if result.returncode != 0:
+            print(f"Command execution failed: {result.stderr}")
+        else:
+            print(f"Command execution successfully: {command}")
+
+    except Exception as e:
+        print(f"Error during command execution: {str(e)}")
+
+
+
+def upgrade_nms(handler, project):
+    # Define the directory names and commands based on the project
+    if project == "MASTER":
+        # Execute the download command using subprocess
+        result = subprocess.run("/home/ubuntu/node_setup/upgrade_nms.sh", shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        # Check the return code to see if the command was successful
+        #if result.returncode != 0:
+        #result =command_run("/home/ubuntu/node_setup/upgrade_nms.sh")
+    else:
+        send_response(handler, 409, "Invalid project name")
+        return
+
+
+    # Start a new thread to run the shell command in the background
+    #thread = threading.Thread(target=command_run, args=(shell_command,))
+    #thread.start()
+    
+    
+    
+                                
+    print(result)
+    if result.returncode != 0:
+        send_response(handler, 500, f"Command execution failed: {result.stderr}")
+
+    # Respond to the client immediately
+    send_response(handler, 201, "Successful project application upgrade.")
+
+
+
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+    
+    def send_error_response(self, status_code, message):
+        self.send_response(status_code)
+        self.end_headers()
+        self.wfile.write(bytes(message, 'utf-8'))
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        data = self.rfile.read(content_length)
+        project = data.decode('utf-8').split("=")[-1]
+
+        if self.path == "/upgrade_nms":
+            upgrade_nms(self, project)
+
+        else:
+            self.send_error_response(404, "Invalid path")
+
+def nms_project_server(host, port):
+    # Create the server with the custom handler
+    with socketserver.TCPServer((host, port), MyHandler) as httpd:
+        print(f"Serving at http://{host}:{port}")
+
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            pass
+
+    print("Server stopped.")
+
+
+if __name__ == "__main__":
+    host = 'localhost'
+    port = 8002
+    nms_project_server(host, port)  # Start the NMS project server
