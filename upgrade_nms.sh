a9f0c4cfc22913cf7b74e7a617f9b315d12a3905
PremUpadhyay55 <142876031+PremUpadhyay55@users.noreply.github.com>
2023-11-09 18:43:51 +0530

    Update upgrade_nms.sh

--- upgrade_nms.sh
+++ upgrade_nms.sh
@@ -1,26 +1,26 @@
 #! /bin/bash -xv
 shopt -s extglob  ## this is to add exclude a folder in a cp command
+cd /home/ubuntu/node_setup
+sudo docker-compose down
+pkill -9 -f "nms_web_server" 
+folder_name="nms"$(date +%Y%m%d%H%M%S)
+mkdir -p /home/ubuntu/node_backup/"$folder_name"
+cp -rp /home/ubuntu/node_setup/ /home/ubuntu/node_backup/"$folder_name"
+git checkout .
+git pull
+wget https://d1xjh92lb8fey3.cloudfront.net/NMS-Update/dev/nms_web_server
+chmod 755 nms_web_server
+sudo docker-compose up -d
+chmod 755 upgrade_nms.sh
+sleep 2
 # Retry up to 3 times
 for attempt in {1..3}; do
-    cd /home/ubuntu/node_setup
-    sudo docker-compose down
-    pkill -9 -f "nms_web_server" 
-    folder_name="nms"$(date +%Y%m%d%H%M%S)
-    mkdir -p /home/ubuntu/node_backup/"$folder_name"
-    cp -rp /home/ubuntu/node_setup/ /home/ubuntu/node_backup/"$folder_name"
-    git checkout .
-    git pull
-    wget https://d1xjh92lb8fey3.cloudfront.net/NMS-Update/dev/nms_web_server
-    chmod 755 nms_web_server
-    sudo docker-compose up -d
-    chmod 755 upgrade_nms.sh
-    sleep 4
-
+	sudo fuser -k 8001/tcp
     # Attempt to start the server
     nohup ./nms_web_server > nms_web_server.log 2>&1 &
     
     # Sleep for a short duration to give the server time to start
-    sleep 2
+    sleep 4
 
     # Check if the server is running on port 8001 using netstat
     if netstat -tulpn | grep :8001; then
