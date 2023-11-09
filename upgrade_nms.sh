857143459fb5eb64ce1b6bf74d4402dec9921ffc
PremUpadhyay55 <142876031+PremUpadhyay55@users.noreply.github.com>
2023-11-09 18:33:09 +0530

    Update upgrade_nms.sh

--- upgrade_nms.sh
+++ upgrade_nms.sh
@@ -1,18 +1,37 @@
 #! /bin/bash -xv
-shopt -s extglob  ## this is to add exlude a folder in a cp command
-cd /home/ubuntu/node_setup
-sudo docker-compose down
-pkill -9 -f "nms_web_server" 
-folder_name="nms"$(date +%Y%m%d%H%M%S)
-mkdir -p /home/ubuntu/node_backup/"$folder_name"
-cp -rp /home/ubuntu/node_setup/ /home/ubuntu/node_backup/"$folder_name"
-git checkout .
-git pull
-wget  https://d1xjh92lb8fey3.cloudfront.net/NMS-Update/dev/nms_web_server
-chmod 755 nms_web_server
-sudo docker-compose up -d
-chmod 755 upgrade_nms.sh
-sleep 4
-nohup ./nms_web_server > nms_web_server.log 2>&1 &
-echo "NMS Upgrade completed Successfully" > /home/ubuntu/node_backup/nms_upgrade.log
+shopt -s extglob  ## this is to add exclude a folder in a cp command
+# Retry up to 3 times
+for attempt in {1..3}; do
+    cd /home/ubuntu/node_setup
+    sudo docker-compose down
+    pkill -9 -f "nms_web_server" 
+    folder_name="nms"$(date +%Y%m%d%H%M%S)
+    mkdir -p /home/ubuntu/node_backup/"$folder_name"
+    cp -rp /home/ubuntu/node_setup/ /home/ubuntu/node_backup/"$folder_name"
+    git checkout .
+    git pull
+    wget https://d1xjh92lb8fey3.cloudfront.net/NMS-Update/dev/nms_web_server
+    chmod 755 nms_web_server
+    sudo docker-compose up -d
+    chmod 755 upgrade_nms.sh
+    sleep 4
 
+    # Attempt to start the server
+    nohup ./nms_web_server > nms_web_server.log 2>&1 &
+    
+    # Sleep for a short duration to give the server time to start
+    sleep 2
+
+    # Check if the server is running on port 8001 using netstat
+    if netstat -tulpn | grep :8001; then
+        echo "NMS Upgrade completed Successfully"
+        break  # Break the loop if the server started successfully
+    else
+        echo "Retry $attempt: NMS Upgrade failed. Retrying..."
+    fi
+done
+
+# If the loop completes without success
+if [ $? -ne 0 ]; then
+    echo "NMS Upgrade failed after multiple attempts."
+fi
