be502772963acdbaf3699427e73ad1c58b2989df
PremUpadhyay55 <142876031+PremUpadhyay55@users.noreply.github.com>
2023-11-16 21:57:51 +0530

    Update upgrade_nms.sh
    added

--- upgrade_nms.sh
+++ upgrade_nms.sh
@@ -6,6 +6,7 @@ cd /home/ubuntu/node_setup
 sudo docker-compose down
 pkill -9 -f "nms_web_server" 
 folder_name="nms"$(date +%Y%m%d%H%M%S)
+sudo rm -f nms_web_server
 mkdir -p /home/ubuntu/node_backup/"$folder_name"
 cp -rp /home/ubuntu/node_setup/ /home/ubuntu/node_backup/"$folder_name"
 git checkout .
