14911f273c5b81135679adbba1bb373d745b3060
PremUpadhyay55 <142876031+PremUpadhyay55@users.noreply.github.com>
2024-01-18 18:40:03 +0530

    Update update_after_reboot.sh
    Added Below Command to update_after_reboot file
    sudo chmod 755 nms_web_server
    sudo chmod 755 upgrade_nms.sh
    sudo chmod 755 upgrade_nms_script.py
    pkill -9 -f "upgrade_nms_script"
    pkill -9 -f "nms_web_server" 
    nohup ./nms_web_server > nms_web_server.log 2>&1 &
    nohup python3 upgrade_nms_script.py >> upgrade_nms_script.log 2>&1 &

--- update_after_reboot.sh
+++ update_after_reboot.sh
@@ -6,9 +6,19 @@ git checkout origin nms.cfg
 sed -i -e 's/localhost/'$IP'/g' nms.cfg
 sudo cp ./nms.cfg /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/nms.cfg /etc/nginx/sites-enabled/
-sudo systemctl start nginx
-sudo docker-compose up -d
+sudo systemctl restart nginx
+sudo docker-compose restart
 sleep 10
+
+sudo chmod 755 nms_web_server
+sudo chmod 755 upgrade_nms.sh
+sudo chmod 755 upgrade_nms_script.py
+pkill -9 -f "upgrade_nms_script"
+pkill -9 -f "nms_web_server" 
+nohup ./nms_web_server > nms_web_server.log 2>&1 &
+nohup python3 upgrade_nms_script.py >> upgrade_nms_script.log 2>&1 &
+
+
 IP_ADDR=$(wget -qO- ifconfig.me) 
 echo "### Node Setup Completed  ##"
 echo " "
@@ -20,10 +30,11 @@ URL="http://$IP_ADDR:8000/nms_app/get_node_id/"
 NODE_ID=$(curl  -s -X 'POST' \
   $URL \
   -H 'accept: application/json' \
-  -H 'authorization: GFA2V8ZQDCORI86ZFDT13G6SYJEXSA93' \
+  -H 'authorization: AR12532DE@#GH&67GF24GH45532##FGG' \
   -d '')
   NODE_ID=$(echo "$NODE_ID" | cut -d ":" -f 3 | sed 's/.$//')
 echo " Your Node id Is:  $NODE_ID "
 #!/bin/bash
 # update the ip to backend
 curl -X POST http://$IP_ADDR:9000/nms_app/ip_update/
+
