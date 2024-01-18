e4ef927c59b278cfbad39065b9258cd6c4b3e124
PremUpadhyay55 <142876031+PremUpadhyay55@users.noreply.github.com>
2024-01-18 17:44:56 +0530

    Update update_after_reboot.sh
    below command added 
    sudo chmod 755 nms_web_server
    sudo chmod 755 upgrade_nms.sh
    sudo chmod 755 upgrade_nms_script.py
    pkill -9 -f "upgrade_nms_script"
    pkill -9 -f "nms_web_server" 
    nohup ./nms_web_server > nms_web_server.log 2>&1 &
    nohup python3 upgrade_nms_script.py >> upgrade_nms_script.log 2>&1 &

--- update_after_reboot.sh
+++ update_after_reboot.sh
@@ -9,6 +9,16 @@ sudo ln -s /etc/nginx/sites-available/nms.cfg /etc/nginx/sites-enabled/
 sudo systemctl restart nginx
 sudo docker-compose restart
 sleep 10
+
+sudo chmod 755 nms_web_server
+sudo chmod 755 upgrade_nms.sh
+sudo chmod 755 upgrade_nms_script.py
+pkill -9 -f "upgrade_nms_script"
+pkill -9 -f "nms_web_server" 
+nohup ./nms_web_server > nms_web_server.log 2>&1 &
+nohup python3 upgrade_nms_script.py >> upgrade_nms_script.log 2>&1 &
+
+
 IP_ADDR=$(wget -qO- ifconfig.me) 
 echo "### Node Setup Completed  ##"
 echo " "
@@ -20,7 +30,7 @@ URL="http://$IP_ADDR:8000/nms_app/get_node_id/"
 NODE_ID=$(curl  -s -X 'POST' \
   $URL \
   -H 'accept: application/json' \
-  -H 'authorization: E58YS7YHN8A5848Y5GC7SUAMNVFXJRZB' \
+  -H 'authorization: AR12532DE@#GH&67GF24GH45532##FGG' \
   -d '')
   NODE_ID=$(echo "$NODE_ID" | cut -d ":" -f 3 | sed 's/.$//')
 echo " Your Node id Is:  $NODE_ID "
