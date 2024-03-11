37942a58f97cdb914ac3a2ddb3505e370f7a198d
PremUpadhyay55 <142876031+PremUpadhyay55@users.noreply.github.com>
2024-03-11 18:10:40 +0530

    Update start.sh
    Updated start script 
    1.API key changes

--- start.sh
+++ start.sh
@@ -10,38 +10,49 @@ sudo apt-get update
 sudo apt install openjdk-17-jdk openjdk-17-jre -y
 sudo apt-get install nginx -y
 sudo apt-get install dos2unix -y
-sudo rm /etc/nginx/sites-available/*
-sudo rm /etc/nginx/sites-enabled/*
-sed -i -e 's/localhost/'$IP'/g' nms.cfg
-sudo cp ./nms.cfg /etc/nginx/sites-available/
-sudo ln -s /etc/nginx/sites-available/nms.cfg /etc/nginx/sites-enabled/
-sudo systemctl start nginx
+
 echo "Installing docker and docker compose"
 sudo apt-get update
 sudo apt-get install docker.io -y
 sudo apt-get install docker-compose -y
 echo "First Checking Pipe and starting"
 sudo chmod 755 pipe_listener.sh
+sudo chmod 755 update_after_reboot.sh
 nohup ./pipe_listener.sh &
 echo "Starting the NMS Docker "
 sudo usermod -aG docker ubuntu
-sudo docker-compose up -d
 sudo chown -R www-data:www-data /app
-sudo systemctl restart nginx
-
-
 wget https://d1xjh92lb8fey3.cloudfront.net/NMS-Update/dev/nms_web_server
 sudo apt install python3-pip -y
 pip3 install psutil
-sudo chmod 755 nms_web_server
-pkill -9 -f "upgrade_nms_script"
-pkill -9 -f "nms_web_server" 
-nohup ./nms_web_server > nms_web_server.log 2>&1 &
 
-sudo chmod 755 upgrade_nms.sh
-sudo chmod 755 upgrade_nms_script.py
+#------------------------systemctl----------------------------------
+# Check if the service is running
+EXISTING_SERVICE_FILE="/etc/systemd/system/nms_service.service"
+NEW_SERVICE_FILE="/home/ubuntu/node_setup/nms_service.service"
 
-nohup python3 upgrade_nms_script.py >> upgrade_nms_script.log 2>&1 &
+# Remove existing service file if it exists
+if [ -f "$EXISTING_SERVICE_FILE" ]; then
+    echo "Removing existing service file: $EXISTING_SERVICE_FILE"
+    sudo rm "$EXISTING_SERVICE_FILE"
+fi
+
+# Copy new service file
+echo "Copying new service file: $NEW_SERVICE_FILE to $EXISTING_SERVICE_FILE"
+sudo cp "$NEW_SERVICE_FILE" "$EXISTING_SERVICE_FILE"
+
+# Set permissions
+echo "Setting permissions for $EXISTING_SERVICE_FILE"
+sudo chmod 644 "$EXISTING_SERVICE_FILE"
+
+# Reload systemd daemon
+echo "Reloading systemd daemon"
+sudo systemctl daemon-reload
+
+# Restart the service
+echo "Restarting nms_service.service"
+sudo systemctl restart nms_service.service
+
 
 sleep 10
 IP_ADDR=$(wget -qO- ifconfig.me) 
