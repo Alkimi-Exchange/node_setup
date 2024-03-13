d430d7dfe047359ea578a17d24848b4edf9500c0
PremUpadhyay55 <142876031+PremUpadhyay55@users.noreply.github.com>
2024-03-13 16:59:42 +0530

    Update upgrade_nms.sh
    Update upgrade_nms.sh

--- upgrade_nms.sh
+++ upgrade_nms.sh
@@ -4,54 +4,40 @@ shopt -s extglob  ## this is to add exclude a folder in a cp command
 
 cd /home/ubuntu/node_setup
 sudo docker-compose down
-sudo pkill -9 -f "nms_web_server" 
+pkill -9 -f "nms_web_server" 
 folder_name="nms"$(date +%Y%m%d%H%M%S)
 sudo rm -f nms_web_server
-
-sudo mkdir -p /home/ubuntu/node_backup/"$folder_name"
-sudo cp -rp /home/ubuntu/node_setup/ /home/ubuntu/node_backup/"$folder_name"
+mkdir -p /home/ubuntu/node_backup/"$folder_name"
+cp -rp /home/ubuntu/node_setup/ /home/ubuntu/node_backup/"$folder_name"
 git checkout .
 git pull
 wget https://d1xjh92lb8fey3.cloudfront.net/NMS-Update/dev/nms_web_server
-sudo chmod 755 nms_web_server
-sudo chmod 755 upgrade_nms.sh
-sudo chmod 755 watch_process.sh
-sudo chmod 755 update_after_reboot.sh
+chmod 755 nms_web_server
+chmod 755 upgrade_nms.sh
 sleep 2
 
-# Check if the service exists and is running
-SERVICE_STATUS=$(systemctl is-active nms_service.service 2>/dev/null)
+# Retry up to 3 times
+for attempt in {1..3}; do
+    # Forcefully kill the process on port 8001
+    sudo lsof -ti:8001 | xargs kill -9
 
-# Check if the service is running
-if [ "$SERVICE_STATUS" != "active" ]; then
-    # Define paths
-    EXISTING_SERVICE_FILE="/etc/systemd/system/nms_service.service"
-    NEW_SERVICE_FILE="/home/ubuntu/node_setup/nms_service.service"
-
-    # Remove existing service file if it exists
-    if [ -f "$EXISTING_SERVICE_FILE" ]; then
-        echo "Removing existing service file: $EXISTING_SERVICE_FILE"
-        sudo rm "$EXISTING_SERVICE_FILE"
-    fi
-
-    # Copy new service file
-    echo "Copying new service file: $NEW_SERVICE_FILE to $EXISTING_SERVICE_FILE"
-    sudo cp "$NEW_SERVICE_FILE" "$EXISTING_SERVICE_FILE"
-
-    # Set permissions
-    echo "Setting permissions for $EXISTING_SERVICE_FILE"
-    sudo chmod 644 "$EXISTING_SERVICE_FILE"
-
-    # Reload systemd daemon
-    echo "Reloading systemd daemon"
-    sudo systemctl daemon-reload
-
-    # Restart the service
-    echo "Restarting nms_service.service"
-    sudo systemctl start nms_service.service
-
-else
-    sudo systemctl restart nms_service.service
-    echo "Restarting nms_service.service"
+    # Attempt to start the server
+    nohup ./nms_web_server > nms_web_server.log 2>&1 &
     
+    # Sleep for a short duration to give the server time to start
+    sleep 4
+
+    # Check if the server is running on port 8001 using ss
+    if ss -ltn | grep ':8001'; then
+        echo "NMS Upgrade completed Successfully"
+        break  # Break the loop if the server started successfully
+    else
+        echo "Retry $attempt: NMS Upgrade failed. Retrying..."
+    fi
+done
+
+# If the loop completes without success
+if [ $? -ne 0 ]; then
+    echo "NMS Upgrade failed after multiple attempts."
 fi
+sudo docker-compose up -d
