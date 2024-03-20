02ecdb804de39977a4b6ee1aa3eb5faf4772a370
PremUpadhyay55 <142876031+PremUpadhyay55@users.noreply.github.com>
2024-03-20 17:16:54 +0530

    Update upgrade_nms.sh
    Update upgrade_nms.sh

--- upgrade_nms.sh
+++ upgrade_nms.sh
@@ -2,7 +2,7 @@
 shopt -s extglob  ## this is to add exclude a folder in a cp command
 cd /home/ubuntu/node_setup
 sudo docker-compose down
-pkill -9 -f "nms_web_server" 
+sudo pkill -9 -f "nms_web_server" 
 folder_name="nms"$(date +%Y%m%d%H%M%S)
 sudo rm -f nms_web_server
 mkdir -p /home/ubuntu/node_backup/"$folder_name"
