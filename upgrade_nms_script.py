19dd7a82be56d17ac140d5ae3ce7bc0b7e8d61c5
PremUpadhyay55 <142876031+PremUpadhyay55@users.noreply.github.com>
2024-04-08 12:03:41 +0530

    Update upgrade_nms_script.py
    Update upgrade_nms_script.py

--- upgrade_nms_script.py
+++ upgrade_nms_script.py
@@ -5,28 +5,32 @@ import socketserver
 import os
 import subprocess
 import json
-import time
+import time 
+import threading
+import multiprocessing
+from http.server import BaseHTTPRequestHandler
+
+
 def send_response(handler, status_code, message):
-    try:
-        # Send the HTTP response status code
-        handler.send_response(status_code)
+    # Send the HTTP response status code
+    handler.send_response(status_code)
 
-        # End the HTTP headers
-        handler.end_headers()
+    # End the HTTP headers
+    handler.end_headers()
 
-        # Create a response body as a dictionary
-        response_body = {
-            'success': True if status_code == 201 else False,
-            'message': message
-        }
-        # Convert the response body to a JSON string
-        response_json = json.dumps(response_body)
+    # Create a response body as a dictionary
+    response_body = {
+        'success': True if status_code == 201 else False,
+        'message': message
+    }
 
-        # Write the JSON response to the client
-        handler.wfile.write(bytes(response_json, 'utf-8'))
+    # Convert the response body to a JSON string
+    response_json = json.dumps(response_body)
 
-    except Exception as e:
-        print(f"Error during execution: {str(e)}")
+    # Write the JSON response to the client
+    handler.wfile.write(bytes(response_json, 'utf-8'))
+
+
 def command_run(command):
     try:
         # Execute the download command using subprocess
@@ -42,6 +46,7 @@ def command_run(command):
         print(f"Error during command execution: {str(e)}")
 
 
+
 def upgrade_nms(handler, project):
     try:
         # Start the subprocess in the background
@@ -52,60 +57,39 @@ def upgrade_nms(handler, project):
     except Exception as e:
         print(f"Error during command execution: {str(e)}")
 
-def setup_systemctl(handler, project):
-    try:
-        # Start the subprocess in the background
-        process = subprocess.Popen("/home/ubuntu/node_setup/setup_systemctl.sh", shell=True)
-        print(f"this is process {process}")
-        # Respond to the client immediately
-        send_response(handler, 201, "setup systemctl process started in the background.")
-    except Exception as e:
-        print(f"Error during command execution: {str(e)}")
+
 
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
+    
     def send_error_response(self, status_code, message):
-        try:
-            self.send_response(status_code)
-            self.end_headers()
-            self.wfile.write(bytes(message, 'utf-8'))
-        except Exception as e:
-            # Log the exception or handle it accordingly
-            print(f"An error occurred while sending error response: {str(e)}")
+        self.send_response(status_code)
+        self.end_headers()
+        self.wfile.write(bytes(message, 'utf-8'))
+
     def do_POST(self):
-        try:
-            content_length = int(self.headers['Content-Length'])
-            data = self.rfile.read(content_length)
-            project = data.decode('utf-8').split("=")[-1]
+        content_length = int(self.headers['Content-Length'])
+        data = self.rfile.read(content_length)
+        project = data.decode('utf-8').split("=")[-1]
 
-            if self.path == "/upgrade_nms":
-                upgrade_nms(self, project)
+        if self.path == "/upgrade_nms":
+            upgrade_nms(self, project)
 
-            elif self.path == "/setup_systemctl":
-                setup_systemctl(self, project)
-
-            else:
-                self.send_error_response(404, "Invalid path")
-        except Exception as e:
-            # Handle any exceptions that occur during processing
-            error_message = f"An error occurred: {str(e)}"
-            self.send_error_response(500, error_message)
-
-
+        else:
+            self.send_error_response(404, "Invalid path")
 
 def nms_project_server(host, port):
-    try:
-        # Create the server with the custom handler
-        with socketserver.TCPServer((host, port), MyHandler) as httpd:
-            print(f"Serving at http://{host}:{port}")
-            try:
-                httpd.serve_forever()
-            except KeyboardInterrupt:
-                pass
-        print("Server stopped.")
-    except Exception as e:
-        # Handle any exceptions that occur during processing
-        print(f"Error during command execution: {str(e)}")
+    # Create the server with the custom handler
+    with socketserver.TCPServer((host, port), MyHandler) as httpd:
+        print(f"Serving at http://{host}:{port}")
+
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            pass
+
+    print("Server stopped.")
+
 
 if __name__ == "__main__":
     host = 'localhost'
