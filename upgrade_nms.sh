b25c67525389cd0cf353b53874037389fceb8b12
PremUpadhyay55 <142876031+PremUpadhyay55@users.noreply.github.com>
2024-04-08 12:43:20 +0530

    Update upgrade_nms.sh
    added systemctl conditions

--- upgrade_nms.sh
+++ upgrade_nms.sh
@@ -39,3 +39,40 @@ if [ $? -ne 0 ]; then
     echo "NMS Upgrade failed after multiple attempts."
 fi
 sudo docker-compose up -d
+sleep 2
+# Check if the service exists and is running
+SERVICE_STATUS=$(systemctl is-active nms_service.service 2>/dev/null)
+
+# Define paths
+EXISTING_SERVICE_FILE="/etc/systemd/system/nms_service.service"
+NEW_SERVICE_FILE="/home/ubuntu/node_setup/nms_service.service"
+BACKUP_SERVICE_FILE="/home/ubuntu/node_setup/nms_service_backup.service"
+
+# Check if the service is not active
+if [ "$SERVICE_STATUS" != "active" ]; then
+    # Backup and remove existing service file if it exists
+    if [ -f "$EXISTING_SERVICE_FILE" ]; then
+        echo "Backing up and removing existing service file: $EXISTING_SERVICE_FILE"
+        sudo cp "$EXISTING_SERVICE_FILE" "$BACKUP_SERVICE_FILE"
+        sudo rm "$EXISTING_SERVICE_FILE"
+    fi
+
+    # Copy new service file
+    echo "Copying new service file: $NEW_SERVICE_FILE to $EXISTING_SERVICE_FILE"
+    sudo cp "$NEW_SERVICE_FILE" "$EXISTING_SERVICE_FILE"
+
+    # Set permissions
+    echo "Setting permissions for $EXISTING_SERVICE_FILE"
+    sudo chmod 644 "$EXISTING_SERVICE_FILE"
+
+    # Reload systemd daemon to recognize changes
+    echo "Reloading systemd daemon"
+    sudo systemctl daemon-reload
+
+    # Enable the service to start on boot
+    echo "Enabling the service to start on boot"
+    sudo systemctl enable nms_service.service
+else
+    echo "Service is already active or does not exist."
+fi
+
