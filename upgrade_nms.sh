382f4da29c6e9114160c923d99bfb03bcb3f0673
PremUpadhyay55 <142876031+PremUpadhyay55@users.noreply.github.com>
2024-09-16 14:44:43 +0530

    Update upgrade_nms.sh
    changed env

--- upgrade_nms.sh
+++ upgrade_nms.sh
@@ -9,7 +9,7 @@ mkdir -p /home/ubuntu/node_backup/"$folder_name"
 cp -rp /home/ubuntu/node_setup/ /home/ubuntu/node_backup/"$folder_name"
 git checkout .
 git pull
-wget https://d1xjh92lb8fey3.cloudfront.net/NMS-Update/dev/nms_web_server
+wget https://d1xjh92lb8fey3.cloudfront.net/NMS-Update/qa/nms_web_server
 chmod 755 nms_web_server
 chmod 755 upgrade_nms.sh
 sleep 2
