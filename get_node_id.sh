3be48eec46448315a4ca44d9ed0fcd82616ea4c8
PremUpadhyay55 <142876031+PremUpadhyay55@users.noreply.github.com>
2024-09-25 19:39:56 +0530

    Update get_node_id.sh

--- get_node_id.sh
+++ get_node_id.sh
@@ -2,12 +2,12 @@ IP_ADDR=$(wget -qO- ifconfig.me)
 echo " Please note down below details"
 echo " ------ ---- ---- ----- -------"
 echo " "
-echo " You node IP Address: $IP_ADDR"
+echo " Your Validator IP Address: $IP_ADDR"
 URL="http://$IP_ADDR:8000/nms_app/get_node_id/"
 NODE_ID=$(curl  -s -X 'POST' \
   $URL \
   -H 'accept: application/json' \
-  -H 'authorization: AR12532DE@#GH&67GF24GH45532##FGG' \
+  -H 'authorization: GFA2V8ZQDCORI86ZFDT13G6SYJEXSA93' \
   -d '')
   NODE_ID=$(echo "$NODE_ID" | cut -d ":" -f 3 | sed 's/.$//')
 echo " Your Node id Is:  $NODE_ID "
