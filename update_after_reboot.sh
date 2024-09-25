5bdc9a6a3c2eae97326bd261ea0ec0349dbb434d
PremUpadhyay55 <142876031+PremUpadhyay55@users.noreply.github.com>
2024-09-25 19:42:58 +0530

    Update update_after_reboot.sh

--- update_after_reboot.sh
+++ update_after_reboot.sh
@@ -83,7 +83,7 @@ echo " "
 echo " Please note down below details"
 echo " ------ ---- ---- ----- -------"
 echo " "
-echo " You node IP Address: $IP_ADDR"
+echo " Your Validator IP Address: $IP_ADDR"
 URL="http://$IP_ADDR:8000/nms_app/get_node_id/"
 NODE_ID=$(curl  -s -X 'POST' \
   $URL \
