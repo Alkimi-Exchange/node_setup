5b32fd748f36da0afbb79398829ffd7e131509ec
PremUpadhyay55 <142876031+PremUpadhyay55@users.noreply.github.com>
2025-01-21 13:14:55 +0530

    Update get_node_id.sh

--- get_node_id.sh
+++ get_node_id.sh
@@ -7,7 +7,7 @@ URL="http://$IP_ADDR:8000/nms_app/get_node_id/"
 NODE_ID=$(curl  -s -X 'POST' \
   $URL \
   -H 'accept: application/json' \
-  -H 'authorization: E58YS7YHN8A5848Y5GC7SUAMNVFXJRZB' \
+  -H 'authorization: NAN' \
   -d '')
   NODE_ID=$(echo "$NODE_ID" | cut -d ":" -f 3 | sed 's/.$//')
 echo " Your Node id Is:  $NODE_ID "
